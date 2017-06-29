53e4ae56758f2fa2161aa1dce659f67492a66dc4
Mário Ribeiro <mail@marioribeiro.com>
2017-06-29 17:45:34 +0100

    Add validations for uniqueness (restaurant name) and ratings (if outside of the range of 1 to 5)

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,4 @@
 class Restaurant < ApplicationRecord
-  validates :name, length: { minimum: 3 }
+  validates :name, length: { minimum: 3 }, uniqueness: true
   has_many :reviews, dependent: :destroy
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
+  it 'is invalid if the rating is more than 5' do
+    review = Review.new(rating: 10)
+    expect(review).to have(1).error_on(:rating)
+  end
+
+  it 'is invalid if the rating is less than 1' do
+    review = Review.new(rating: 0)
+    expect(review).to have(1).error_on(:rating)
+  end
 end
