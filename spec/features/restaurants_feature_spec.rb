6481d36c4060a1c09fcfd8795cf9fba11d01e826
Mário Ribeiro <mail@marioribeiro.com>
2017-06-29 12:51:21 +0100

    Add test for restaurant creation

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -20,4 +20,15 @@ feature 'restaurants' do
       expect(page).not_to have_content('No restaurants available')
     end
   end
+
+  context 'creating restaurants' do
+    scenario 'prompts user to fill out a form, then display the new restaurant' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'Frankie'
+      click_button 'Create restaurant'
+      expect(page).to have_content('Frankie')
+      expect(current_path).to eq '/restaurants'
+    end
+  end
 end
